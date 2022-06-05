cbe0b32f4e0d9656e5a6d469985f8a2e06f278b8
stanislavl25 <stanislavlysytsia08@gmail.com>
2022-06-06 00:48:22 +0100

    update

--- src/components/NutritionInfoCA.jsx
+++ src/components/NutritionInfoCA.jsx
@@ -1,27 +1,126 @@
-import { Button, Card, Heading, Stack, TextField } from "@shopify/polaris";
+import {
+  Button,
+  Card,
+  FormLayout,
+  Heading,
+  Popover,
+  Select,
+  Stack,
+  TextField,
+} from "@shopify/polaris";
 import React, { useState, useCallback } from "react";
-import SelectComponent from "./SelectComponent";
-import PopOverComponent from "./PopOverComponent";
+
+const PopOverComponent = ({
+  handleChange,
+  index,
+  element,
+  removeFormFields,
+}) => {
+  const [popoverActive, setPopoverActive] = useState(false);
+
+  const togglePopoverActive = useCallback(
+    () => setPopoverActive((popoverActive) => !popoverActive),
+    []
+  );
+
+  const activator = (
+    <Button onClick={togglePopoverActive} disclosure>
+      More
+    </Button>
+  );
+  const options = [
+    { label: "Yes", value: "Yes" },
+    { label: "No", value: "No" },
+  ];
+  return (
+    <Popover
+      active={popoverActive}
+      activator={activator}
+      onClose={togglePopoverActive}
+      ariaHaspopup={false}
+      sectioned
+      autofocusTarget="none"
+      preferredAlignment="center"
+    >
+      <FormLayout>
+        <Stack>
+          <Stack.Item fill>
+            <Select
+              onChange={(e) => handleChange(e, "bold", index)}
+              value={element.bold || ""}
+              options={options}
+              name="bold"
+              label="Bold Name"
+            />
+          </Stack.Item>
+          <Stack.Item>
+            <TextField
+              label="Order"
+              type="number"
+              name="order"
+              min={0}
+              value={element.order || ""}
+              onChange={(e) => {
+                handleChange(e, "order", index);
+              }}
+              multiline={false}
+            />
+          </Stack.Item>
+        </Stack>
+        <TextField
+          label="Left Spacing (Table)"
+          type="number"
+          value={element.leftSpacing || ""}
+          min={0}
+          name="leftSpacing"
+          onChange={(e) => handleChange(e, "leftSpacing", index)}
+          multiline={false}
+        />
+        <div>
+          <Heading>Prepared Product</Heading>
+          <TextField
+            label="% DV*"
+            type="number"
+            value={element.preparedProduct || ""}
+            min={0}
+            name="% RI*"
+            onChange={(e) => handleChange(e, "preparedProduct", index)}
+            multiline={false}
+          />
+        </div>
+        <Button
+          destructive
+          outline
+          style={{ margin: "4px" }}
+          type="button"
+          className="button remove"
+          onClick={() => {
+            removeFormFields(index);
+            togglePopoverActive();
+          }}
+        >
+          Delete
+        </Button>
+      </FormLayout>
+    </Popover>
+  );
+};
+const newFormSet = {
+  name: "",
+  quantity: "",
+  Unit: "Grams",
+  dailyValue: "",
+  bold: "No",
+  order: "",
+  leftSpacing: "",
+  preparedProduct: "",
+};
 function NutritionInfoCA({
   formValues,
   setFormValues,
   formLables,
-  handleOrderChange,
-  newFormSet,
   locationPlan,
 }) {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const handlePopOverOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopOverClose = () => {
-    setAnchorEl(null);
-  };
-
-  const openPopOver = Boolean(anchorEl);
-  const id = openPopOver ? "simple-popover" : undefined;
-
   let addFormFields = () => {
     setFormValues([...formValues, newFormSet]);
   };
@@ -30,25 +129,17 @@ function NutritionInfoCA({
     let newFormValues = [...formValues];
     newFormValues.splice(i, 1);
     setFormValues(newFormValues);
-    handlePopOverClose();
   };
   let handleSave = async (event) => {
     event.preventDefault();
     console.log(JSON.stringify(formValues));
   };
-  const handleChange = useCallback((e, i, name) => {
+  const handleChange = useCallback((e, name, i) => {
     console.log(e);
     let newFormValues = [...formValues];
     newFormValues[i][name] = e;
     setFormValues(newFormValues);
   }, []);
-
-  const handleUnitChange = (i, val) => {
-    let newFormValues = [...formValues];
-    newFormValues[i]["Unit"] = val;
-    setFormValues(newFormValues);
-  };
-
   const options = [
     { label: "Grams", value: "Grams" },
     { label: "MilliGrams", value: "MilliGrams" },
@@ -89,46 +180,35 @@ function NutritionInfoCA({
               >
                 <TextField
                   style={{ width: "130px", marginRight: "10px" }}
-                  value={element.name}
-                  name="Name"
-                  onChange={(e) => handleChange(e, index, "name")}
-                  label=""
+                  value={element.name || ""}
+                  name="name"
+                  onChange={(e) => handleChange(e, "name", index)}
                   autoComplete="off"
                 />
                 <TextField
                   style={{ width: "80px", marginRight: "10px" }}
                   value={element.quantity || 0}
-                  name="Per100g"
-                  onChange={(e) => handleChange(e, index, "per100g")}
+                  name="quantity"
+                  onChange={(e) => handleChange(e, "quantity", index)}
                   inputMode="number"
                   autoComplete="off"
                 />
                 <div style={{ width: "100px", marginRight: "10px" }}>
-                  <SelectComponent
-                    index={index}
-                    unit={element.unit}
-                    handleChange={handleChange}
+                  <Select
+                    value={element.Unit || ""}
+                    onChange={(e) => handleChange(e, "Unit", index)}
                     options={options}
-                    name="unit"
+                    name="Unit"
                   />
                 </div>
                 <TextField
-                  style={{ width: "120px", marginRight: "10px" }}
                   value={element.dailyValue || 0}
-                  name="Perportion"
-                  onChange={(e) => handleChange(e, index, "perportion")}
+                  name="dailyValue"
+                  onChange={(e) => handleChange(e, "dailyValue", index)}
                   autoComplete="off"
                   inputMode="number"
                 />
-                <Button onClick={handlePopOverOpen} aria-describedby={id}>
-                  More
-                </Button>
                 <PopOverComponent
-                  openPopOver={openPopOver}
-                  handleOrderChange={handleOrderChange}
-                  id={id}
-                  anchorEl={anchorEl}
-                  handlePopOverClose={handlePopOverClose}
                   element={element}
                   handleChange={handleChange}
                   removeFormFields={removeFormFields}
