4931da556a9a7d67b8ae5c4a5ccd1142412e4bce
stanislavl25 <stanislavlysytsia08@gmail.com>
2023-11-28 02:20:30 +0100

    Update restart_app.sh

--- restart_app.sh
+++ restart_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Stopping any existing node servers"
 cd /htdocs/nutrition-table
-sudo chown -R bitnami:bitnami
+sudo chown -R bitnami:bitnami *
 pkill node || true
 nohup yarn start </dev/null &>/dev/null &
