62c9aebb38630dd88d212a8fd4d299bdf096d5aa
stanislavl25 <stanislavlysytsia08@gmail.com>
2023-11-30 02:24:08 +0100

    Update restart_app.sh

--- restart_app.sh
+++ restart_app.sh
@@ -7,5 +7,7 @@ rm -rf node_modules
 rm yarn.lock
 yarn install
 yarn
-nohup yarn start </dev/null &>/dev/null &
+# nohup yarn start </dev/null &>/dev/null &
+nohup yarn start >> /var/log/myapp-deploy.log 2>&1 &
+
 echo "AfterInstall hook finished" >> /var/log/myapp-deploy.log
