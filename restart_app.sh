e781c2d5a3ed47f9e2b4ecc2fe31b715faf9967c
stanislavl25 <stanislavlysytsia08@gmail.com>
2023-11-30 02:56:28 +0100

    Update restart_app.sh

--- restart_app.sh
+++ restart_app.sh
@@ -4,6 +4,6 @@ cd /home/bitnami/htdocs/nutrition-table
 sudo chown -R bitnami:bitnami *
 rm -rf node_modules
 rm yarn.lock
-npm install >> /var/log/myapp-deploy.log 2>&1
+sudo -u bitnami yarn install >> /var/log/myapp-deploy.log 2>&1
 # yarn
 # nohup yarn start </dev/null &>/dev/null &
