fafcd44b289428bada486c922b51fe98c24e1541
stanislavl25 <stanislavlysytsia08@gmail.com>
2023-11-30 16:28:56 +0100

    Update application_start.sh

--- codedeploy-hooks/application_start.sh
+++ codedeploy-hooks/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pkill node || true
 cd /home/bitnami/htdocs/nutrition-table
-sudo -u bitnami nohup yarn start >> /var/log/myapp-deploy.log 2>&1
+sudo -u bitnami nohup yarn start >> /var/log/myapp-deploy.log 2>&1 &
