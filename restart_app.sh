532b937e554f0c11aff6826289add81e49d8dc20
stanislavl25 <stanislavlysytsia08@gmail.com>
2023-11-30 02:12:59 +0100

    Update restart_app.sh

--- restart_app.sh
+++ restart_app.sh
@@ -3,6 +3,9 @@ echo "AfterInstall hook started" >> /var/log/myapp-deploy.log
 pkill node || true
 cd /home/bitnami/htdocs/nutrition-table
 sudo chown -R bitnami:bitnami *
+rm -rf node_modules
+rm yarn.lock
+yarn install
 yarn
 nohup yarn start </dev/null &>/dev/null &
 echo "AfterInstall hook finished" >> /var/log/myapp-deploy.log
