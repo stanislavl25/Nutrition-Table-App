4e92b1bc414628d77426a1b9de1e0ef02e873a3b
stanislavl25 <stanislavlysytsia08@gmail.com>
2023-12-10 21:47:34 +0100

    Update application_start.sh

--- codedeploy-hooks/application_start.sh
+++ codedeploy-hooks/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill node || true
-cd /home/ec2-user/nutrition-table
+cd /home/bitnami/htdocs/nutrition-table
 npm install
-cross-env NODE_ENV=production node server/index.js >> /home/ec2-user/myapp-deploy.log 2>&1 &
+npm run serve >> /home/bitnami/myapp-deploy.log 2>&1 &
